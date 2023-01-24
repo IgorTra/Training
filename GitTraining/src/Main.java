65af94745b249bfee967e66867212d24de0be98f
igor <you@example.com>
2023-01-24 13:59:05 +0100

    "New class NewClass"

--- GitTraining/src/Main.java
+++ GitTraining/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello this is file from git");
+
+        NewClass nowa = new NewClass(5);
+
+
     }
 }
