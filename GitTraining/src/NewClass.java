47615666bb0edfc3b3b2741d6daffb68463d0f00
igor <you@example.com>
2023-01-24 14:00:53 +0100

    Changes in class NewClass

---
+++ GitTraining/src/NewClass.java
@@ -0,0 +1,12 @@
+public class NewClass {
+
+    private final int number;
+
+    public NewClass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
